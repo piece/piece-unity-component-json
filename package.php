d515ace152ceeb183733ca79dbf26a45046d4db0
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-16 11:11:13 +0000

    - Updated.

--- package.php
+++ package.php
@@ -73,7 +73,7 @@ $package->setReleaseStability($releaseStability);
 $package->setNotes($notes);
 $package->setPhpDep('4.3.0');
 $package->setPearinstallerDep('1.4.3');
-$package->addPackageDepWithChannel('required', 'Piece_Unity', 'pear.piece-framework.com', '0.13.0');
+$package->addPackageDepWithChannel('required', 'Piece_Unity', 'pear.piece-framework.com', '1.0.0');
 $package->addPackageDepWithChannel('optional', 'HTML_AJAX', 'pear.php.net', '0.5.0');
 $package->addExtensionDep('optional', 'json');
 $package->addPackageDepWithChannel('optional', 'PHPUnit', 'pear.phpunit.de', '1.3.2', '1.3.2');
