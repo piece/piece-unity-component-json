9f404b01acf97ccf9cf8d520d190c6e511bf94c3
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-18 10:08:52 +0000

    - A minor improvement.

--- tests/Piece/Unity/Plugin/Renderer/JSONTestCase.php
+++ tests/Piece/Unity/Plugin/Renderer/JSONTestCase.php
@@ -36,7 +36,7 @@
  * @since      File available since Release 1.0.0
  */
 
-require dirname(__FILE__) . '/../../../../prepare.php';
+require_once realpath(dirname(__FILE__) . '/../../../../prepare.php');
 require_once 'PHPUnit.php';
 require_once 'HTML/AJAX/JSON.php';
 require_once 'Piece/Unity/Plugin/Renderer/JSON.php';
